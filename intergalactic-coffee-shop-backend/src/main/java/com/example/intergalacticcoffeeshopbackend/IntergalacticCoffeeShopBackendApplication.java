fe1154ad3d17f0cd177db1ef5723086b294432e3
Florescu Alexandru <68075327+flowAlexS@users.noreply.github.com>
2026-08-27 17:04:19 +0300

    init backend proj

---
+++ intergalactic-coffee-shop-backend/src/main/java/com/example/intergalacticcoffeeshopbackend/IntergalacticCoffeeShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.intergalacticcoffeeshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergalacticCoffeeShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergalacticCoffeeShopBackendApplication.class, args);
+	}
+
+}
